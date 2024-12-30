1cfa62aa6fbf39e168888060327c5915342677b0
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-12-30 21:18:20 +0530

    Setup Clerk And Initial Layout of Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-	return (
-		<div>
-			<main>
-				<h1>Lets an AI Productivity App </h1>
-			</main>
-		</div>
-	);
+  return (
+    <div>
+      <main>
+        <h1>Lets an AI Productivity App </h1>
+        <Button>Click Me</Button>
+      </main>
+    </div>
+  );
 }
